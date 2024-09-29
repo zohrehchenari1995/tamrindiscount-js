85e6bda56712cd1ed3b543497dc53379a88bb421
zohrehchenari1995 <zohrehchenari2874@gmail.com>
2024-09-29 21:14:01 +0330

    added capitalize statement

--- index.js
+++ index.js
@@ -1,47 +1,23 @@
-// INCREMENT
-function increment(arrayNum , number){
 
-  let newArray = [];
-  for(const item of arrayNum){
-    newArray.push(item + number);
-  }
-  return newArray;
+
+// !capitalize satement
+const userName = "sAHeb"; 
+
+function capitalizeFirstLetter (string){
+  const lowerCaseString = string.toLowerCase();
+  const newString = lowerCaseString[0].toUpperCase() + lowerCaseString.slice(1);
+  return newString;
 }
-const resultNewArray = increment([2,4,6],2);
-console.log(resultNewArray);
+console.log(capitalizeFirstLetter("sAHeb"));
 
-
-
-// DECREMENT
-function decrement(arr ,num){
-  let array = [];
-  for(const index of arr){
-    array.push(index - num);
-  }
-  return array;
+function capitalizeStatemant(string){
+  const name = string.split(" ");
+  const newNames = [];
+  for(const n of name){
+    newNames.push(capitalizeFirstLetter(n));
+    return newNames.join(" ");
+  };
+  
 }
-const resultDecrement = decrement([10,8,6],3);
-console.group(resultDecrement);
-
- 
-// ! دوتا تابع فوق را با استفاده از تابع مرتبه بالا و کال بک میایم و از تکرارشون جلوگیری میکنیم
-
-// call back function
-function inc(item,number){
-  return item + number;
-}
-function dec(item,number){
-  return item - number;
-}
-
-function operationOnArray (data,cb,number){
-  const newArray = [];
-  for(const item of data){
-    newArray.push(cb(item,number));
-  }
-  return newArray;
-}
-const resultInc = operationOnArray([5,7,9],inc,2);
-const resultDec = operationOnArray([10,9,8],dec,3);
-console.log(resultInc);
-console.log(resultDec);
+const message = "the dogs,dogs are my favorite pet";
+console.log(capitalizeStatemant(message));
