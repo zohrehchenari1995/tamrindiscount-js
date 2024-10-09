ca66e470cbafc9c0514376cf4462f191c8b081be
zohrehchenari1995 <zohrehchenari2874@gmail.com>
2024-10-09 19:59:35 +0330

    added copy event

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
 
-const cart = [
-  {
-    id:1,
-    title:"p-1",
-    price:100,
-  },
-  {
-    id:2,
-    title:"p-2",
-    price:110,
-  },
-  {
-    id:3,
-    title:"p-3",
-    price:120,
-  },
-  {
-    id:4,
-    title:"p-4",
-    price:130,
-  },
-]
+ const eventInput = document.querySelector("#search-value");
 
-const enRolledCart = [1,2];
-
-function checkedEnrolled (carts, enRolled){
-
-   const cartCourseIds = carts.map((getIdCart)=> { return getIdCart.id});
-   const found = enRolled.some((item)=> { return cartCourseIds.includes(item)});
-   if(found) {
-    return "your course is exist"
-  };
-   return "your successfuly regidter";
-}
-console.log(checkedEnrolled(cart,enRolledCart))
+ eventInput.addEventListener("copy",(eventCopy)=>{
+    console.log(`${eventCopy.type} : ${document.getSelection()}`);
+ });
