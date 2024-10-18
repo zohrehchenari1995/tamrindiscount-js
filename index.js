86bc6b981e160b9df24002efdb70f95b0b092f6c
zohrehchenari1995 <zohrehchenari2874@gmail.com>
2024-10-18 12:54:45 +0330

    added editbutton

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-// !متد show چک شود
-// const showModal = document.querySelector(".show-modal");
-// const backdrop = document.querySelector(".backdrop");
-// const modal = document.querySelector(".modal");
-// const closeModal = document.querySelector(".close-modal")
 
 
-// function show(){
-//   backdrop.classList.remove("hidden");
-//   modal.classList.remove("hidden");
-// }
-// function closebuttonModal(){
-//   backdrop.classList.add("hidden");
-//   modal.classList.add("hidden");
-// }
-// backdrop.addEventListener("click",show);
-// modal.addEventListener("click",show);
-// backdrop.addEventListener("click",closebuttonModal);
-// modal.addEventListener("click",closebuttonModal);
+// let todos = [];
+let filterValue ="all";
 
-
-// form => submit (event) =>+,enter,form
-// crete new todo {id,title,createdAt,,iscompleted(false)}
-// save to array => const newTodo = []
-
-
-const todos = [];
 const searchInput = document.querySelector(".search__input");
 const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todoList");
 const selectFilter = document.querySelector(".filter__todos");
 
 searchInput.addEventListener("submit",addNewTodos);
-selectFilter.addEventListener("change",filterTodo);
+selectFilter.addEventListener("change",(e)=>{
+  filterValue =e.target.value;
+  filterTodo();
+});
+document.addEventListener("DOMContentLoaded",(e)=>{
+  let todos = getAllTodos();
+  createdTodos(todos);
+})
+
 
 function addNewTodos(e){
   e.preventDefault();
@@ -42,52 +28,133 @@ function addNewTodos(e){
     title :todoInput.value,
     isCompleted :false, 
   }
-  todos.push(newTodo);
-  createdTodos(todos)
+  // todos.push(newTodo);
+  saveTodos(newTodo);
+  filterTodo();
 };
 
 // created  newtodo
 function createdTodos(todos){
   let result ="";
   todos.forEach((todo)=>{
-   
-     result += `
+    
+    result += `
     <li class="todo">
-    <p class="todo__title">${todo.title}</p>
+    <p class="todo__title  ${todo.isCompleted && "completed"}">${todo.title}</p>
     <span class="todo__createAt">${new Date(todo.createdAt).toLocaleDateString("fa-IR")}</span>
-    <button data-todo-id = ${todo.id}>
-    <i class="todo__checked  fa-solid fa-square-check"></i>
+
+    <button  data-todo-id = ${todo.id} class="todo__edit">
+    <i class='fas'>&#xf044;</i>
     </button>
-    <button data-todo-id = ${todo.id}>
-    <i class="todo__trash     fa-solid fa-trash-can"></i>
+    <div class="backdrop  hidden"></div>
+    <div class="modal  hidden">
+      <div class="modal__content">
+        <input type="text" name="text">
+        <button class="modal__submitEdit">edit</button>
+        <button class="close__modal">x</button>
+      </div>
+    </div>
+
+    <button data-todo-id = ${todo.id} class="todo__checked">
+      <i class="fa-solid fa-square-check"></i>
+    </button>
+    <button data-todo-id = ${todo.id}  class="todo__trash">
+      <i class="fa-solid fa-trash-can"></i>
     </button>
     </li> `;
-    todoList.innerHTML = result;
-    todoInput.value = "";
+   
   });
+  todoList.innerHTML = result;
+  todoInput.value = "";
+
+  // select and addEvent for remove
+  const todoTrash =[...document.querySelectorAll(".todo__trash")];
+  todoTrash.forEach((btn)=>{
+    btn.addEventListener("click",trashTodo);
+  })
+  // select and addEvent for check 
+  const todoChecked = [...document.querySelectorAll(".todo__checked")];
+  todoChecked.forEach((btns)=>{
+    btns.addEventListener("click",checkTodo);
+  })
+
+  //! select and addEvent for edit
+
+  const editButton = [...document.querySelectorAll(".todo__edit")];
+  editButton.forEach((btnEdit)=>{
+    btnEdit.addEventListener("click",showModal);
+  })
+  const backdropBac = document.querySelector(".backdrop");
+  const modal = document.querySelector(".modal");
+
+  backdropBac.addEventListener("clikc",backDropModal);
+  modal.addEventListener("click",modalInput)
 };
 
-
-function filterTodo(e){
-  const filter = e.target.value;
-  switch (filter){
+function filterTodo(){
+  let todos = getAllTodos();
+  switch (filterValue){
     case "all" :{
       createdTodos(todos);
       break;
     }
     case "completed" :{
-      const filteredTodos = todos.filter((t)=>t.isCompleted);
+      const filteredTodos = todos.filter((t)=>{ return t.isCompleted});
       createdTodos(filteredTodos);
       break;
     }
     case "uncompleted" :{
-      const filteredTodos = todos.filter((t)=> !t.isCompleted);
+      const filteredTodos = todos.filter((t)=>{return ! t.isCompleted});
     createdTodos(filteredTodos);
     break;
     }
     default : createdTodos(todos)
   };
 }
+function trashTodo(e){
+  let todos = getAllTodos();
+  const todoId = Number(e.target.dataset.todoId);
+  todos = todos.filter((t)=>{return  t.id !== todoId;});
+  saveAllTodos(todos);
+  filterTodo();
+}
+function checkTodo(e){
+  let todos = getAllTodos();
+  const todoIdCheck = Number(e.target.dataset.todoId);
+  const findTodo = todos.find((t)=>t.id === todoIdCheck);
+  findTodo.isCompleted = !findTodo.isCompleted;
+  saveAllTodos(todos);
+  filterTodo();
+  
+}
+// !function showModal
+function showModal(){
+  backdropBac.classList.remove("hidden");
+  modal.classList.remove("hidden");
+}
+function backDropModal(){
+  backdrop.classList.add("hidden");
+  modal.classList.add("hidden");
+}
+function modalInput(){
+  backdrop.classList.add("hidden");
+  modal.classList.add("hidden");
+}
 
 
+// localstorage
+function getAllTodos(){
+  const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+  return savedTodos;
+}
 
+function saveTodos(todo){
+  const savedTodos = getAllTodos();
+  savedTodos.push(todo);
+  localStorage.setItem("todos",JSON.stringify(savedTodos));
+  return savedTodos;
+}
+
+function saveAllTodos (todos){
+  localStorage.setItem("todos",JSON.stringify(todos));
+}
